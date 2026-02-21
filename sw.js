378ca9adaa55d4eb75cc8c6f2ae3f1b22d90da63
placeholdertemporary <vscprk@proton.me>
2026-02-21 23:02:05 +0000

    Implement basic fetch event listener in service worker
    Added a basic fetch event listener to the service worker.

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-
+// Basic service worker to enable PWA installation
+self.addEventListener('fetch', (event) => {
+  // We don't need to cache anything yet, 
+  // but the event listener must exist.
+});
